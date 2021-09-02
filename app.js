604279e790666f4c05dfb51be620982e312976f8
rslick <rslick@ciis.canon.com>
2021-09-01 17:50:03 -0700

    Simulate universal link back to our application

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 'use strict';
 
 const express = require('express');
-var fs = require('fs');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to the Webcam Subscription Server').end();
+  res.status(200).sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get('/.well-known/apple-app-site-association', (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.status(200).send(fs.readFileSync('./.well-known/apple-app-site-association', {encoding: 'utf-8'})).end();
+  res.redirect('/v1/api/token')
+});
+
+app.get('/get-token', (req, res) => {
+  // TODO: We would go through the login process with the Canon subscription server here
+
+  // The following redirect will redirect back to our Mac application using universal links
+  res.redirect('/v1/api/token')
+});
+
+app.get('/v1/api/token', (req, res) => {
+  res.status(200).send('<h2>Temporary Page</h2>We were redirected here via a <i>Location</i> header.<br>Accessing this URL should have caused a return to our native application using universal links.').end();
 });
 
 // Start the server
