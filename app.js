cace45617571478477e7aef84f18f0fe90926f7b
rslick <rslick@ciis.canon.com>
2021-11-22 18:04:26 -0800

    Updated to test login flow using only the webcam server

--- app.js
+++ app.js
@@ -17,14 +17,23 @@ app.get('/.well-known/apple-app-site-association', (req, res) => {
 });
 
 app.get('/get-token', (req, res) => {
-  // TODO: We would go through the login process with the Canon subscription server here
+  // We will go through the login process with the Canon subscription server here, which will
+  // redirect back to our Mac application using universal links, after successful authentication.
+  res.redirect('https://ccb-dev.com/canonid/#/login?client_id=a560247f-c16c-43cb-8ce1-aa466395df7d&redirect_uri=https:%2F%2Feos-webcam-dev.wl.r.appspot.com%2Fwebcam%2Fauthtoken&rd=1&scope=openid&response_type=id_token&nonce=some_value')
+});
 
-  // The following redirect will redirect back to our Mac application using universal links
+app.get('/webcam/authtoken', (req, res) => {
+  // TODO: This URL should probably be /v1/api/token or webcam/v1/api/token
+  // Successful login at Canon Global ID redirects back to this URL.
+  // The authorization code is attached to the redirect URL as a fragment.
+
+  // The following redirect will eventually redirect back to our Mac application using universal links
+  // For now, it just comes back to this server to emulate the behavior and test the authorization flow.
   res.redirect('/v1/api/token/simulate_success')
 });
 
 app.get('/v1/api/token/simulate_success', (req, res) => {
-  res.status(200).send('<h2>Oops!</h2>We were redirected here via a <i>Location</i> header.<br>This URL should have been captured by our native application using universal links.').end();
+  res.status(200).send('<h2>Success!</h2>We were redirected here via a <i>Location</i> header.<br>This URL will eventually be captured by our native application using universal links.').end();
 });
 
 // Start the server
